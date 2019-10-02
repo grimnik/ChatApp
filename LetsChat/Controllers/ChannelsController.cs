1a0663eb4d720ef6971eaeab866f402ff5a0243b
dieter vermander <vermanderdieter@hotmail.com>
2019-10-02 17:01:32 +0200

    Made CreateChannel form and routes to it.
    WIP need to pass trough Id from the chosen Group to return to Group overview and show the new channel.

---
+++ LetsChat/Controllers/ChannelsController.cs
@@ -0,0 +1,33 @@
+﻿using LetsChat.Data;
+using LetsChat.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading.Tasks;
+
+namespace LetsChat.Controllers
+{
+    public class ChannelsController : Controller
+    {
+        public ApplicationDbContext _appContext { get; set; }
+        public ChannelsController(ApplicationDbContext applicationDb)
+        {
+            _appContext = applicationDb;
+        }
+        public IActionResult CreateChannel()
+        {
+            return View();
+        }
+        public IActionResult CreateChannel(ChannelsCreateViewModel model)
+        {
+            _appContext.Channels.Add(new Domain.Channel()
+            {
+                Naam = model.Naam
+            });
+            _appContext.SaveChanges();
+            return RedirectToAction("Channels","Home",);
+        }
+    }
+}
